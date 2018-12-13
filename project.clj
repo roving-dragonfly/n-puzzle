c5d9326d574611c1a37faf0bb13613d3c31539ab
rd <roving.dragonfly@gmail.com>
2018-12-14 00:59:32 +0100

    Main and cli interaction

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject n-puzzle "1.0"
   :description "Solver of taquin puzzle"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/roving-dragonfly/n-puzzle"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.priority-map "0.0.10"]
-                 [quil "2.8.0"]])
+                 [quil "2.8.0"]
+                 [org.clojure/tools.cli "0.4.1"]]
+  :main ^:skip-aot n-puzzle.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- src/n_puzzle/core.clj
+++ src/n_puzzle/core.clj
@@ -1,22 +1,59 @@
 (ns n-puzzle.core
-  (:require [n-puzzle.solver :refer [solvable? A* euclidian-distance manhattan-distance]]
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :refer [as-file]]
+            [n-puzzle.solver :refer [solvable?]]
             [n-puzzle.parser :refer [parse]]
             [n-puzzle.generator :refer [generate-from-solved generate-puzzle]]
-            [n-puzzle.visualisator :refer [draw-puzzle]]))
+            [n-puzzle.visualisator :refer [draw-puzzle]])
+  (:gen-class))
 
-(defn file->map
-  "Get parsed map from file"
-  [file]
-  (-> (slurp file)
-      parse))
 
-(defn full-process
-  "Solves a puzzle map then draws its solution"
-  [puzzle heuristic]
-  (if (solvable? puzzle)
-    (draw-puzzle (A* puzzle heuristic))
-    (println "Not solvable")))
+(defn print-puzzle
+  "Prints a puzzle state"
+  [puzzle]
+  (let [lines (partition (:size puzzle) (:puzzle puzzle))]
+    (doseq [line lines]
+      (println (map #(format "%2d" %) line)))))
 
-;; Some commands to tryout
+(defn print-solution
+  "Prints a solution"
+  [solution]
+  (println (str "Solution took " (:moves solution) " moves"))
+  (println (str "Frontier expanded to " (apply max (:frontier solution))) "potentials paths")
+  (println "Solution from initial state to solved state")
+  (doseq [puzzle (:solution solution)]
+    (print-puzzle {:size (:size solution) :puzzle puzzle})
+    (println "")))
 
-(full-process (generate-puzzle 5) manhattan-distance)
+(def cli-options
+  (let [algos ["A*"]
+        heuristics ["manhattan-distance" "euclidian-distance"]] 
+    [["-a" "--algorithm ALGORITHM" "Algorithm used"
+      :default "A*"
+      :validate [(fn [e] (prn e) (some #(= % e) algos)) "This is not a valid algorithm"]]
+     ["-h" "--heuristic HEURISTIC" "Heuristic used"
+      :default "manhattan-distance"
+      :validate [(fn [e] (prn e) (some #(= % e) heuristics)) "This is not a valid heuristic"]]
+     ["-v" "--visualisator" "Solution visualisator"
+      :default true
+      :parse-fn not]
+     ["-m" "--map MAP" "Input map"
+      :default nil]]))
+
+(defn -main [& args]
+  (let [args (parse-opts args cli-options)
+        filename (get-in args [:options :map])
+        file (when (and filename (.exists (as-file filename))) (slurp filename))]
+    (if-let [err (:errors args)]
+      (prn err)
+      (let [map (if file (parse file) (generate-puzzle 4))
+            algo  (ns-resolve (find-ns 'n-puzzle.solver)
+                              (symbol (get-in args [:options :algorithm])))
+            heuristic  (ns-resolve (find-ns  'n-puzzle.solver)
+                                   (symbol (get-in args [:options :heuristic])))
+            solution (when (solvable? map) (algo map heuristic))]
+        (cond
+          (nil? solution) (println "Not solvable")
+          (get-in args [:options :visualisator]) (draw-puzzle solution)
+          :else (print-solution solution))))))
+
